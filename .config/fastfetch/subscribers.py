9fe5406c3e2f5d0d4ed5266cde45ccb4e5676cd6
Zipux <167661757+ImZipux@users.noreply.github.com>
2024-05-21 12:17:34 -0400

    Create subscribers.py

---
+++ .config/fastfetch/subscribers.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+
+CHANNELID = "CHANNEL ID GOES HERE"
+KEY      = "KEY GOES HERE"
+
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+CHANNELID+"&key="+KEY).read()
+
+subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+
+print(f"{subs}")
